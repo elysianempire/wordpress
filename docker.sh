b17814abeed5fe4e4011dcab20c9c081d955c5d7
Gabriel Kwok <gkwok@nukenin.com>
2016-02-19 14:51:09 -0800

    Added docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose up -d
